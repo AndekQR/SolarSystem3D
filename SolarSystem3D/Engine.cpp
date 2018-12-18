d73d23a8c6bf534f9d523840902bd92109d000e3
AndekQR <daniellegawiec20@gmail.com>
2018-12-18 12:50:32 +0100

    Dodanie timera

--- SolarSystem3D/Engine.cpp
+++ SolarSystem3D/Engine.cpp
@@ -5,8 +5,10 @@
 
 #define ASPECT_1_1 1
 
-Move move;
+#define FPS 50
+
 bool isMazeIsDrawn = false;
+Move* move;
 
 GLfloat alfa = 0.1;
 GLfloat beta = 0.2;
@@ -15,29 +17,14 @@ GLfloat beta = 0.2;
 void display() {
 	
 
-	move.angle = move.angle + move.turn;
-
-	//Update the camera position based on the current camera speeds
-	move.xPos = move.xPos + move.speed * sin(move.angle);
-	move.zPos = move.zPos + move.speed * cos(move.angle);
-
-	//update the look-at position based on the current cam position
-	move.xLookAt = (float)(move.xPos + sin(move.angle));
-	move.zLookAt = (float)(move.zPos + cos(move.angle));
-
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); //czyszczenie buforu koloru i z-buforu
 	
 	glClearColor(1.0, 1.0, 1.0, 1.0); //bialy
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
 
-	gluLookAt(move.xPos, 0.5, move.zPos, move.xLookAt, 0.5, move.zLookAt, 0, 1, 0);
+	gluLookAt(move->xPos, 0.5, move->zPos, move->xLookAt, 0.5, move->zLookAt, 0, 1, 0);
 	//gluLookAt(move.xPos, 10, move.zPos, move.xLookAt, 0.5, move.zLookAt, 0, 1, 0); //widok z gory
-
-	// kolor krawêdzi szeœcianu
-	//glColor3f(0.0, 0.0, 0.0);
-
-	//glutWireCube(10);
 
 	glColor3f(1, 1, 0);
 	glutSolidSphere(5, 12, 12);
@@ -69,34 +56,29 @@ void display() {
 
 }
 
-void obroty() {
+
+
+void timer(int val) {
+
 	alfa = alfa + 0.1;
 	beta = beta + 0.1;
+
+	move->angle = move->angle + move->turn;
+
+	//aktualizacja pozycji kamery na podstawie prêdkoœci kamery
+	move->xPos = move->xPos + move->speed * sin(move->angle);
+	move->zPos = move->zPos + move->speed * cos(move->angle);
+
+	//aktualizacja kierunku patrzenia kamery na podstawie jej pozycji
+	move->xLookAt = (float)(move->xPos + sin(move->angle));
+	move->zLookAt = (float)(move->zPos + cos(move->angle));
+
 	glutPostRedisplay();
+	glutTimerFunc(1000 / FPS, timer, 0);
 }
 
 void reshape(int width, int height) {
-	/*
-	glViewport(0, 0, width, height); //obszar renderingu ca³e okno
 
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-
-	if (ASPECT_1_1 == 1) { //parametry bry³y obcinania
-		if (width >= height && height>0) {
-			glFrustum(-2.0 * width / height, 2.0 * width / height, -2.0, 2.0, 1.0, 5.0);
-		}
-		else {
-			if (height > width && width>0) {
-				glFrustum(-2.0, 2.0, -2.0 * height / width, 2.0 * height / width, 1.0, 5.0);
-			}
-		}
-	}
-	else {
-		glFrustum(-2.0, 2.0, -2.0, 2.0, 1.0, 5.0);
-	}
-
-	*/
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
 	gluPerspective(75, 1, 0.01, 100);
@@ -120,19 +102,19 @@ void specialKeys(int key, int x, int y) {
 	switch (key)
 	{
 	case GLUT_KEY_UP:
-		move.moveForward(true);
+		move->moveForward(true);
 		display();
 		break;
 	case GLUT_KEY_DOWN:
-		move.moveBackward(true);
+		move->moveBackward(true);
 		display();
 		break;
 	case GLUT_KEY_LEFT:
-		move.turnLeft(true);
+		move->turnLeft(true);
 		display();
 		break;
 	case GLUT_KEY_RIGHT:
-		move.turnRight(true);
+		move->turnRight(true);
 		display();
 		break;
 	default:
@@ -144,16 +126,16 @@ void specialUpKeys(int key, int x, int y) {
 	switch (key)
 	{
 	case GLUT_KEY_UP:
-		move.moveForward(false);
+		move->moveForward(false);
 		break;
 	case GLUT_KEY_DOWN:
-		move.moveBackward(false);
+		move->moveBackward(false);
 		break;
 	case GLUT_KEY_LEFT:
-		move.turnLeft(false);
+		move->turnLeft(false);
 		break;
 	case GLUT_KEY_RIGHT:
-		move.turnRight(false);
+		move->turnRight(false);
 		break;
 	default:
 		break;
@@ -185,7 +167,10 @@ int main(int argc, char *argv[]) {
 	glutMotionFunc(mouseMotion); //obsluga ruchu myszki
 	glutSpecialFunc(specialKeys); //strzalki
 	glutSpecialUpFunc(specialUpKeys);//obsluga puszczenia klawiszy 
-	glutIdleFunc(obroty); //obroty w tle
+	glutTimerFunc(1000 / FPS, timer, 0); //timer
+
+	move = new Move();
+	move->setPos(40, 40);
 
 	glEnable(GL_DEPTH_TEST);//wlaczenie bufora z
 	//glDepthFunc(GL_GEQUAL);
